fdb67d701d22a5ba4bbd2252ce56e685043b8466
刘白菜 <liushuai.baicai@hotmail.com>
2017-10-27 13:31:51 +0800

    列表详细信息

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bingImage = require('../bingimage');
+var dateFormat = require('dateformat');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,8 +9,18 @@ router.get('/', function(req, res, next) {
         res.render('index', {
             title: "There are " + images.count + " images!",
             images: images.rows,
+            dateFormat: dateFormat,
+            parse: parse,
         });
     })
 });
+
+function parse(str) {
+    var y = str.substr(0,4),
+        m = str.substr(4,2) - 1,
+        d = str.substr(6,2);
+    var D = new Date(y,m,d);
+    return (D.getFullYear() == y && D.getMonth() == m && D.getDate() == d) ? D : 'invalid date';
+}
 
 module.exports = router;
