5dd9d9cd39e3e4cc3a08666a1bfd50a831b560b2
刘白菜 <liushuai.baicai@hotmail.com>
2017-10-27 15:40:47 +0800

    壁纸下载

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var bingImage = require('../bingimage');
 var dateFormat = require('dateformat');
+var fs = require('fs');
+
+var bingHost = 'http://www.bing.com';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +15,34 @@ router.get('/', function(req, res, next) {
             dateFormat: dateFormat,
             parse: parse,
         });
-    })
+    });
+});
+
+router.get('/today', function(req, res, next) {
+    bingImage.findOne({ order: [ [ 'endDate', 'DESC' ] ] }).then(savedImage => {
+        var rootPath = './public/data'
+        var bingName = `${savedImage.urlBase.split('/')[ savedImage.urlBase.split('/').length-1]}_1920x1080.jpg`;
+        var bingPath = `${rootPath}/image/${bingName}`;
+        fs.createReadStream(bingPath).pipe(res);
+    });
+});
+
+router.get('/download', function (req, res, next) {
+    var id = req.query.key;
+    bingImage.findById(id).then(savedImage => {
+        if (savedImage==null){
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        }else{
+            savedImage.downloadCount++;
+            savedImage.save();
+            var rootPath = './public/data'
+            var bingName = `${savedImage.urlBase.split('/')[ savedImage.urlBase.split('/').length-1]}_1920x1080.jpg`;
+            var bingPath = `${rootPath}/image/${bingName}`;
+            res.download(bingPath, bingName);
+        }
+    });
 });
 
 function parse(str) {
