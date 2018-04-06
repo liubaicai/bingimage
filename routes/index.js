cabc75f53b9b6967ec51fc53376569890889672d
刘白菜 <liushuai.baicai@hotmail.com>
2018-04-06 13:59:46 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bingImage = require('../bingimage');
+var request = require('request');
 var dateFormat = require('dateformat');
 var fs = require('fs');
 
@@ -49,6 +50,12 @@ router.get('/download', function (req, res, next) {
     });
 });
 
+router.get('/start', function (req, res, next) {
+    bingImage.findOne({ where: {startDate: dateFormat(new Date(), 'yyyymmdd')} }).then(savedImage => {
+        startDownload(savedImage);
+    })
+});
+
 function parse(str) {
     var y = str.substr(0,4),
         m = str.substr(4,2) - 1,
@@ -57,4 +64,58 @@ function parse(str) {
     return (D.getFullYear() == y && D.getMonth() == m && D.getDate() == d) ? D : 'invalid date';
 }
 
+function startDownload(savedImage) {
+    request(`${bingHost}/HPImageArchive.aspx?format=js&idx=-1&n=1`, function (error, response, body) {
+        var rootPath = './public/data'
+        var publicRootPath = '/data'
+        var rootObj = JSON.parse(body).images[0];
+        var bingUrlBase = `${bingHost}${rootObj.urlbase}`;
+        var bingUrl = `${bingUrlBase}_1920x1080.jpg`;
+        var bingThumbUrl = `${bingUrlBase}_320x240.jpg`;
+        var bingName = bingUrl.split('/')[ bingUrl.split('/').length-1];
+        var bingThumbName = bingThumbUrl.split('/')[ bingThumbUrl.split('/').length-1];
+        var bingPath = `${rootPath}/image/${bingName}`
+        var bingThumbPath = `${rootPath}/thumb/${bingThumbName}`
+
+        if(!fs.existsSync(`${rootPath}/thumb/${bingThumbName}`)){
+            request(bingUrl).pipe(fs.createWriteStream(`${bingPath}.tmp`, {flags: 'w+'}))
+                .on('close',function(){
+                    request(bingThumbUrl).pipe(fs.createWriteStream(`${bingThumbPath}.tmp`, {flags: 'w+'}))
+                        .on('close',function(){
+                            fs.renameSync(`${bingPath}.tmp`, bingPath);
+                            fs.renameSync(`${bingThumbPath}.tmp`, bingThumbPath);
+                            if(savedImage != null){
+                                savedImage.update({
+                                    thumbnailUrl: `${publicRootPath}/thumb/${bingThumbName}`,
+                                    downloadUrl: `${publicRootPath}/image/${bingName}`,
+                                }).then(image => {
+                                    console.log('update:'+image.get({
+                                        plain: true
+                                    }).endDate)
+                                })
+                            } else {
+                                bingImage.create({
+                                    startDate: rootObj.startdate,
+                                    fullStartDate: rootObj.fullstartdate,
+                                    endDate: rootObj.enddate,
+                                    url: rootObj.url,
+                                    urlBase: rootObj.urlbase,
+                                    copyright: rootObj.copyright,
+                                    copyrightLink: rootObj.copyrightlink,
+                                    thumbnailUrl: `${publicRootPath}/thumb/${bingThumbName}`,
+                                    downloadUrl: `${publicRootPath}/image/${bingName}`,
+                                }).then(image => {
+                                    console.log('create:'+image.get({
+                                        plain: true
+                                    }).endDate)
+                                })
+                            }
+                        });
+                });
+        }
+    });
+}
+
 module.exports = router;
+
+
